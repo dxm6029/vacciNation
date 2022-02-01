8665dbd021a026bd51f8e27de6de3948ef290df1
lo6169 <lo6169@rit.edu>
2022-01-31 21:01:27 -0500

    Notices page is completed, similar structure willbe used for FAQ

--- frontend/mainapp/src/routes.js
+++ frontend/mainapp/src/routes.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Routes, Route, Navigate} from 'react-router-dom';
 
 import Home from './components/Home';
+import Notices from './components/notices';
 import Schedule from './components/schedule';
+import FAQ from './components/faq';
 
 /**
  * All routes go here.
@@ -13,7 +15,9 @@ class UBRoutes extends React.Component {
         return (
             <Routes>
                 <Route path="/home" element={<Home />} />
+                <Route path="/notices" element={<Notices information={this.props.information}/>}/>
                 <Route path="/schedule" element={<Schedule/>}/>
+                <Route path="/faq" element={<FAQ/>} />
             </Routes>
         );
     }
