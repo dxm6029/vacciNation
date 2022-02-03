f118d91b2fa7c89fa15ffece2fc3e10f6e012cc8
dominique-molee <dxm6029@rit.edu>
2022-02-02 23:43:55 -0500

    logging

--- backend/VacciNationAPI/DataLayer/User.cs
+++ backend/VacciNationAPI/DataLayer/User.cs
@@ -195,7 +195,7 @@ namespace VacciNationAPI.DataLayer
                     result = true;
                 }
 
-            } catch (Exception e){ } // probably should log something here eventually
+            } catch (Exception e){ Console.WriteLine(e.Message); Console.WriteLine(e.StackTrace);} // probably should log something here eventually
             finally{
                connection.CloseConnection(conn);
             }
