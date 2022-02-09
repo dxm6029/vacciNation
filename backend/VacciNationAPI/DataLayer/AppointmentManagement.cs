7994ed997c00e574a18b4e2bbaa3da09e5ae0a49
dominique-molee <dxm6029@rit.edu>
2022-02-09 18:16:00 -0500

    debug

--- backend/VacciNationAPI/DataLayer/AppointmentManagement.cs
+++ backend/VacciNationAPI/DataLayer/AppointmentManagement.cs
@@ -42,53 +42,31 @@ namespace VacciNationAPI.DataLayer
         public int updateTimeslotCitizenDose(Timeslot timeslot, int vaccineType){
             int result = -1;
             MySqlConnection conn = connection.OpenConnection();
-
+            MySqlTransaction myTrans =  conn.BeginTransaction();
             try{
-
-                // // check to make sure a dose of that type is in the dose table - need to add to make sure it is available 
-                // string query = "SELECT dose_id FROM dose WHERE vaccine_id=@vaccineType";
-                // MySqlCommand cmd = new MySqlCommand(query, conn);
-                // cmd.Parameters.AddWithValue("@vaccineType", vaccineType);
-
-                // MySqlDataReader rdr = cmd.ExecuteReader();
-                // int availableDoseID = -1;
-
-                // // loop through dose of appropriate type
-                // while (rdr.Read())
-                // {   
-                //     // check to make sure dose is not already taken
-                //     string availCheck = "SELECT dose_id FROM timeslot WHERE dose_id=@doseId AND status = @stat";
-                //     MySqlCommand comm = new MySqlCommand(availCheck, conn);
-                //     comm.Parameters.AddWithValue("@doseId", rdr.GetInt32(0));
-                //     comm.Parameters.AddWithValue("@stat", 1);
-
-                //     MySqlDataReader rdr2 = cmd.ExecuteReader();
-                //     if(rdr2.Read()){
-                //         availableDoseID = rdr.GetInt32(0);
-                //     }
-                // }
-                // rdr.Close();
-
-                // // there are no doses of that type available at the moment
-                // if(availableDoseID == -1){
-                //     return -2;
-                // }
-
-                // update the timeslot to inlude that citizen and dose
-                string updateSlot = "BEGIN; SELECT @local := dose.dose_id FROM dose LEFT JOIN timeslot on dose.dose_id = timeslot.dose_id WHERE vaccine_id=@vaccine_id AND timeslot_id IS NULL LIMIT 1 FOR UPDATE; Update timeslot set dose_id = IF(ISNULL(@local), dose_id, @local), citizen_id=@citizen_id, status_id=@statusNum WHERE timeslot_id = @timeslot_id; COMMIT;";
+                string updateSlot = "SELECT @local := dose.dose_id FROM dose LEFT JOIN timeslot on dose.dose_id = timeslot.dose_id WHERE vaccine_id=@vaccine_id AND timeslot_id IS NULL LIMIT 1 FOR UPDATE; Update timeslot set dose_id = IF(ISNULL(@local), dose_id, @local), citizen_id=@citizen_id, status_id=@statusNum WHERE timeslot_id = @timeslot_id;";
                 MySqlCommand cd = new MySqlCommand(updateSlot, conn);
+               
+                cd.Transaction = myTrans;
+
+                myTrans = conn.BeginTransaction();
                 cd.Parameters.AddWithValue("@vaccine_id", vaccineType);
                 cd.Parameters.AddWithValue("@citizen_id", timeslot.citizen_id);
                 cd.Parameters.AddWithValue("@statusNum", 2);
                 cd.Parameters.AddWithValue("@timeslot_id", timeslot.timeslot_id);
 
                 int rows = cd.ExecuteNonQuery();
+                
+                myTrans.Commit();
 
                 if(rows <= 0){
                     return -2;
                 }
 
-            } catch (Exception e){ Console.WriteLine(e.Message); Console.WriteLine(e.StackTrace);} // probably should log something here eventually
+            } catch (Exception e){ 
+                myTrans.Rollback();
+                Console.WriteLine(e.Message); 
+                Console.WriteLine(e.StackTrace);} // probably should log something here eventually
             finally{
                connection.CloseConnection(conn);
             }
