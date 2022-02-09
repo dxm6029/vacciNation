bd8fa29b978d7f9a121df49bc219f87ec9383ef4
lo6169 <lo6169@rit.edu>
2022-02-08 21:24:41 -0500

    Report and schedule more fleshed out.

--- frontend/mainapp/src/pages/report.js
+++ frontend/mainapp/src/pages/report.js
@@ -18,6 +18,27 @@ function Report() {
         />
       </div>
 
+      <h1> Report a Reaction </h1>
+
+      <label for="firstname">Patient's First Name:</label>
+      <input type="text" id="firstname" name="firstname" placeholder="First Name"></input>
+
+      <label for="lastname">Patient's Last Name:</label>
+      <input type="text" id="lastname" name="lastname" placeholder="Last Name"></input>
+
+      <label for="vaccineDate">Date of Vaccine:</label>
+      <input type="date" id="vaccineDate" name="vaccineDate" placeholder="MM/DD/YYYY"></input>
+
+      <label for="reactionDate">Date of Reaction:</label>
+      <input type="date" id="reactionDate" name="reactionDate" placeholder="MM/DD/YYYY"></input>
+
+      <label for="startTime">Time of Reaction: </label>
+      <input type="time" id="startTime"></input>
+
+      <label for="vaccineType">Vaccine Received</label>
+      <select name="vaccineType">
+          <option> Value </option>
+      </select>
       
     </div>
   );
--- frontend/mainapp/src/pages/schedule.js
+++ frontend/mainapp/src/pages/schedule.js
@@ -4,6 +4,10 @@ import NavBar from './navBar';
 
 function Schedule() {
 
+  const tableClick = () => {
+    // Here's where the user will select the row in the table
+  }
+
   return (
     <div className="scheduler">
       <NavBar 
@@ -17,8 +21,29 @@ function Schedule() {
         />
 
         <div className="schedule">
-            Schedule <br/>
-            Here is information.
+            <h1> Schedule Appointment </h1>
+
+            <label for="zipcode">Enter Zipcode:</label>
+            <input type="text" id="zipcode" name="Zipcode"></input>
+
+            <table>
+              <tr>
+                <th>Location</th>
+                <th>Distance</th>
+              </tr>
+              <tr>
+                
+              Loop through more things here
+              </tr>
+            </table>
+
+            <>
+              Calendar to go here <br/>
+            </>
+
+            <>
+              Select a time
+            </>
         </div>
 
         <Link to="/personalInfo">
