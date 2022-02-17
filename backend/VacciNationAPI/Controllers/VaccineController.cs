ac420922e6947d10ed40d00dd9ff7f0512d004a1
Gwen Bewley <lsb3562@rit.edu>
2022-02-16 19:48:54 -0500

    Add create, read, update rest points for vaccine table

--- backend/VacciNationAPI/Controllers/VaccineController.cs
+++ backend/VacciNationAPI/Controllers/VaccineController.cs
@@ -13,6 +13,7 @@ namespace VacciNationAPI.Controllers
         User us = new User();
         VaccineManagement vm = new VaccineManagement();
         
+        //get full list of vaccines
         [HttpGet("all")]
         public IActionResult GetAllVaccines(){
 
@@ -25,5 +26,91 @@ namespace VacciNationAPI.Controllers
             }
         }
         
+        // Creating a new vaccine
+        [HttpPost]
+        public IActionResult CreateVaccine([FromBody] Vaccine vaccine,  [FromHeader] string authorization) {
+            string token = authorization;
+            int uid = us.checkToken(token);
+            if (uid == -1){
+                return Unauthorized();
+            }
+
+            bool isAdmin = us.isSuperAdmin(uid);
+            if(!isAdmin){
+                return StatusCode(403);
+            }
+            
+            // sql call to add timeslot
+            bool result = vm.InsertVaccine(vaccine);
+
+            if(result){
+                return Accepted();
+            }
+
+            return BadRequest();
+        }
+
+        // Updating an existing vaccine
+        [HttpPut] 
+        public IActionResult UpdateVaccine([FromBody] Vaccine vaccine,  [FromHeader] string authorization){
+            try{
+                string token = authorization;
+                int uid = us.checkToken(token);
+                if (uid == -1){
+                    return Unauthorized();
+                }
+
+                //walled behind admin
+                bool isAdmin = us.isSuperAdmin(uid);
+                if(!isAdmin){
+                    return StatusCode(403);
+                }
+
+                //if no attributes are set, nothing should happen
+                if (vaccine.category_id == 0 && vaccine.disease_id == 0 && string.IsNullOrEmpty(vaccine.description))
+                {
+                    return BadRequest();
+                }
+                
+                // should include category, disease, or description
+                bool result = vm.updateVaccine(vaccine);
+
+                if(result){
+                    return Accepted();
+                }else {
+                    return BadRequest();
+                }
+            }
+            catch(Exception e){
+                return BadRequest();
+            }
+        }
+
+        // Get vaccines by either category or disease
+        [HttpGet]
+        public IActionResult GetAllVaccinesOfType(){
+
+            int vaccineCategory = -1;
+            if (!String.IsNullOrEmpty(HttpContext.Request.Query["category_id"])){
+                vaccineCategory = Int32.Parse(HttpContext.Request.Query["category_id"]);
+            }
+
+            int vaccineDisease = -1;
+            if (!String.IsNullOrEmpty(HttpContext.Request.Query["disease_id"])){
+                vaccineDisease = Int32.Parse(HttpContext.Request.Query["disease_id"]);
+            }
+
+            if(vaccineCategory == -1 && vaccineDisease == -1){
+                return BadRequest();
+            }
+
+            try{                
+                List<string> vaccines = vm.getAllVaccinesByType(vaccineCategory, vaccineDisease);
+                return new ObjectResult(vaccines);
+            }
+            catch(Exception e){
+                return BadRequest();
+            }
+        }
     }
 }
--- backend/VacciNationAPI/DataLayer/VaccineManagement.cs
+++ backend/VacciNationAPI/DataLayer/VaccineManagement.cs
@@ -35,5 +35,124 @@ namespace VacciNationAPI.DataLayer
             return vaccines;
         }
         
+        public bool InsertVaccine(Vaccine vaccine){
+            bool result = false;
+            MySqlConnection conn = connection.OpenConnection();
+
+            try{
+                string query = "INSERT INTO vaccine (category, disease, description) VALUES(@category, @disease, @description)";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@category", vaccine.category_id);
+                cmd.Parameters.AddWithValue("@disease", vaccine.disease_id);
+                cmd.Parameters.AddWithValue("@description", vaccine.description);
+                
+                int numAffected = cmd.ExecuteNonQuery();
+
+                if(numAffected > 0){
+                    result = true;
+                }
+
+            } catch (Exception e){ Console.WriteLine(e.Message); Console.WriteLine(e.StackTrace);} // probably should log something here eventually
+            finally{
+                connection.CloseConnection(conn);
+            }
+            return result;
+
+        }
+        
+        public bool updateVaccine(Vaccine vaccine)
+        {
+            bool result = false;
+            MySqlConnection conn = connection.OpenConnection();
+            
+            try{
+                //build update string with whatever is set
+                string query="Update vaccine set  ";
+
+                if (vaccine.category_id != 0) {
+                    query += "category=@category_id, ";
+                }
+
+                if (vaccine.disease_id != 0) {
+                    query += "disease=@disease_id, ";
+                }
+
+                if (!string.IsNullOrEmpty(vaccine.description)) {
+                    query += "description=@description, ";
+                }
+
+                // remove trailing ,
+                if (query[query.Length - 2] == ',')
+                {
+                    query = query.Remove(query.Length - 2);
+                }
+                else
+                {
+                    //none of the values are set, so nothing should happen
+                    return false;
+                }
+
+                query += " WHERE vaccine_id = @vaccine_id;";
+    
+                
+                MySqlCommand cd = new MySqlCommand(query, conn);
+                if (vaccine.category_id != 0) {
+                    cd.Parameters.AddWithValue("@category_id", vaccine.category_id);
+                }
+
+                if (vaccine.disease_id != 0) {
+                    cd.Parameters.AddWithValue("@disease_id", vaccine.disease_id);
+                }
+
+                if (!string.IsNullOrEmpty(vaccine.description)) {
+                    cd.Parameters.AddWithValue("@description", vaccine.description);
+                }
+                cd.Parameters.AddWithValue("@vaccine_id", vaccine.vaccine_id);
+
+                int rows = cd.ExecuteNonQuery();
+
+                if(rows > 0){
+                    result = true;
+                }
+
+            } catch (Exception e){ 
+                Console.WriteLine(e.Message); 
+                Console.WriteLine(e.StackTrace);} // probably should log something here eventually
+            finally{
+                connection.CloseConnection(conn);
+            }
+
+            return result;
+        }
+
+        public List<string> getAllVaccinesByType(int categoryID, int diseaseID){
+            MySqlConnection conn = new MySqlConnection();
+            List<string> vaccines = new List<string>();
+            try{ 
+                conn = connection.OpenConnection();
+
+                string query = "SELECT v.vaccine_id, c.name as category, d.name as disease, v.description FROM vaccine as v LEFT JOIN vaccine_category as c ON v.category = c.category_id LEFT JOIN vaccine_disease as d ON v.disease = d.disease_id WHERE v.category=@category OR v.disease=@disease;";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@category", categoryID);
+                cmd.Parameters.AddWithValue("@disease", diseaseID);
+
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {   
+                    vaccines.Add("{ vaccine_id: " + (rdr.IsDBNull(0) ? -1 : rdr.GetInt32(0)) + ", category: " + ( rdr.IsDBNull(1) ? -1 : rdr.GetString(1)) + ", disease: " + (rdr.IsDBNull(2) ?  "" : rdr.GetString(2))+  ", description: " +  (rdr.IsDBNull(3) ?  "" : rdr.GetString(3)) +"}");
+                }
+                rdr.Close();
+
+            }catch (Exception e){Console.WriteLine(e.Message); Console.WriteLine(e.StackTrace);}
+            finally{
+                connection.CloseConnection(conn);
+            }
+            
+            return vaccines;
+        }
+
     }
 }
