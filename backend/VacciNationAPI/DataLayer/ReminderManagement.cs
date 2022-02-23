9fa89423bb811e4f670da916b98d519ef6647891
dominique-molee <dxm6029@rit.edu>
2022-02-22 23:27:01 -0500

    console logs

--- backend/VacciNationAPI/DataLayer/ReminderManagement.cs
+++ backend/VacciNationAPI/DataLayer/ReminderManagement.cs
@@ -22,7 +22,7 @@ namespace VacciNationAPI.DataLayer
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress("dmolee@gmail.com", "VacciNation");
             var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("dmolee@gmail.com", "VacciNation");
+            var to = new EmailAddress("dxm6029@rit.edu", "Dominique Molee");
             var plainTextContent = "and easy to do anywhere, even with C#";
             var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
