3c78b8d34bcd1a3898e816bf33de181912f49d1e
dominique-molee <dxm6029@rit.edu>
2022-02-24 23:09:59 -0500

    date formatting test

--- backend/VacciNationAPI/DataLayer/ReminderManagement.cs
+++ backend/VacciNationAPI/DataLayer/ReminderManagement.cs
@@ -16,17 +16,17 @@ namespace VacciNationAPI.DataLayer
         VacciNation.Connect connection = new VacciNation.Connect();
 
 
-        public static async Task Execute()
+        public static async Task Execute(string email, string date)
         {
             var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
             Console.WriteLine(apiKey);
 
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress("vaccineschedulingassistant@gmail.com", "Vaccination Scheduling Support");
-            var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("dxm6029@rit.edu", "Dominique Molee");
-            var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var subject = "Reminder: Vaccine Appointment Upcoming";
+            var to = new EmailAddress(email, "Citizen");
+            var plainTextContent = "Hello, <br />";
+            var htmlContent = "We are just reaching out to remind you that your appointment is at <strong> </strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             Console.WriteLine("email created");
 
@@ -43,7 +43,7 @@ namespace VacciNationAPI.DataLayer
                 
             Console.WriteLine("execute");
 
-             //   Execute().Wait();
+               // Execute().Wait();
             Console.WriteLine("execute finished");
 
 
@@ -51,21 +51,36 @@ namespace VacciNationAPI.DataLayer
                 //conn = connection.OpenConnection();
               
                 var dateOnly = date.Date.ToShortDateString();
-                Console.WriteLine(dateOnly);
+                var start = dateOnly + " 00:00:00";
+                var end = dateOnly + " 23:59:59";
 
-                // string query = "SELECT email FROM citizen JOIN timeslot USING(citizen_id) WHERE date > @start AND date < @end";
-                // MySqlCommand cmd = new MySqlCommand(query, conn);
-                // cmd.Parameters.AddWithValue("@start", start);
-                
+                string query = "SELECT email FROM citizen, location.name, vaccine_category.name, vaccine_disease.name, dose.supplier, date JOIN timeslot USING(citizen_id) JOIN location USING(location_id) JOIN dose USING(dose_id) JOIN vaccine USING(vaccine_id) JOIN vaccine_category ON vaccine_category.category_id=vaccine.category JOIN vaccine_disease ON vaccine_disease.disease_id=vaccine.disease WHERE date > @start AND date < @end";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@start", start);
+                cmd.Parameters.AddWithValue("@end", end);
 
-                // MySqlDataReader rdr = cmd.ExecuteReader();
+                MySqlDataReader rdr = cmd.ExecuteReader();
 
-                // while (rdr.Read())
-                // {   
-                //     // should not hit 0, but for consistency
-                //     suppliers.Add(rdr.IsDBNull(0) ?  "" : rdr.GetString(0));
-                // }
-                // rdr.Close();
+                while (rdr.Read())
+                {   
+                    string email = rdr.IsDBNull(0) ?  "" : rdr.GetString(0);
+                    string location = rdr.IsDBNull(1) ?  "" : rdr.GetString(1);
+                    string category = rdr.IsDBNull(2) ?  "" : rdr.GetString(2);
+                    string disease = rdr.IsDBNull(3) ?  "" : rdr.GetString(3);
+                    string supplier = rdr.IsDBNull(4) ?  "" : rdr.GetString(4);
+                    string apptDate = rdr.IsDBNull(5) ?  "" : rdr.GetString(5);
+                    Console.WriteLine(email);
+                    Console.WriteLine(location);
+                    Console.WriteLine(category);
+                    Console.WriteLine(disease);
+                    Console.WriteLine(supplier);
+                    Console.WriteLine(apptDate);
+
+
+                    //Execute(email, dateOnly).Wait();
+                    //suppliers.Add(rdr.IsDBNull(0) ?  "" : rdr.GetString(0));
+                }
+                rdr.Close();
 
             }catch (Exception e){ Console.WriteLine(e.Message); Console.WriteLine(e.StackTrace); }
             finally{
