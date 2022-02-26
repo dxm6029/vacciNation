26439f0de241d490168cc1b16d708d2aa223a157
candidate.Brantley.Wyche.ext <bxw5869@rit.edu>
2022-02-25 21:13:12 -0500

    trying again

--- frontend/mainapp/src/components/loginComponent.js
+++ frontend/mainapp/src/components/loginComponent.js
@@ -4,41 +4,41 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function LoginComponent(props) {
-  const handleSubmit = async(event) => {
-    console.log("logging in")
-        function setFailure() {
-            document.getElementById("FormStatus").innerText = "Form Submission Failed, Please Try Again";
-        }
+//   const handleSubmit = async(event) => {
+//     console.log("logging in")
+//         function setFailure() {
+//             document.getElementById("FormStatus").innerText = "Form Submission Failed, Please Try Again";
+//         }
 
-        let username = event.target.username.value;
-        let password = event.target.password.value;
-        console.log(username);
-        console.log(password);
-        console.log(" ^ password and username ");
-        event.preventDefault()
-        const response = await fetch('http://192.168.1.5:5000/UserStaff/login', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                 'username': username,
-                 'password': password,
-            })
-        })
-        const responseJSON = await response.json()
-        console.log("response " + responseJSON);
-        if (response.status == 202) {
-            console.log("login successful")
-        }
-        if (response.status > 200) {
-            console.log("Login failure: " + response.status)
+//         let username = event.target.username.value;
+//         let password = event.target.password.value;
+//         console.log(username);
+//         console.log(password);
+//         console.log(" ^ password and username ");
+//         event.preventDefault()
+//         const response = await fetch('http://192.168.1.5:5000/UserStaff/login', {
+//             method: 'POST',
+//             headers: {
+//                 'Accept': 'application/json',
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({
+//                  'username': username,
+//                  'password': password,
+//             })
+//         })
+//         const responseJSON = await response.json()
+//         console.log("response " + responseJSON);
+//         if (response.status == 202) {
+//             console.log("login successful")
+//         }
+//         if (response.status > 200) {
+//             console.log("Login failure: " + response.status)
             
-            return
-        }
+//             return
+//         }
 
-  }
+//   }
 
   function LOGIN(user, pass) {
     return axios
