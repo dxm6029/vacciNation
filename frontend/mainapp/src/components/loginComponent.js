07f6328824ed9433d0323f92a6623e114b89c92b
dominique-molee <dxm6029@rit.edu>
2022-03-01 17:10:36 -0500

    adding headers

--- frontend/mainapp/src/components/loginComponent.js
+++ frontend/mainapp/src/components/loginComponent.js
@@ -42,7 +42,19 @@ function LoginComponent(props) {
 
   function LOGIN() {
     return axios
-        .get("http://192.168.1.5:5000/UserStaff/all", {headers: {'Content-Type': 'application/json', "Cache-Control": "no-cache, no-store, must-revalidate", "Pragma": "no-cache", "Expires": 0}})
+        .post("http://192.168.1.5:5000/UserStaff/login", 
+        {
+          headers: {
+          'Content-Type': 'application/json', 
+          "Cache-Control": "no-cache, no-store, must-revalidate", 
+          "Pragma": "no-cache", 
+          "Expires": 0
+          },
+          body: JSON.stringify({
+            'username': username,
+            'password': password,
+          })
+        })
         .then((response) => {
             if (response) {
                 console.log(response); 
