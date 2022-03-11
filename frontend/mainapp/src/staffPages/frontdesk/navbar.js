e989b5253d6b3688b24397faa97b84ae8d326163
lo6169 <lo6169@rit.edu>
2022-03-10 19:26:19 -0500

    More front desk things such as search, updates to home, and navbar.

--- frontend/mainapp/src/staffPages/frontdesk/navbar.js
+++ frontend/mainapp/src/staffPages/frontdesk/navbar.js
@@ -1,10 +1,20 @@
 import './navbar.css';
+import { Link } from 'react-router-dom';
 
 function FrontDeskNavBar() {
     // Front desk navbar
   return (
     <>
-        
+        <div className="nurseheader">
+          <div className='name'>
+            NAME | Report adverse Reaction
+          </div>
+           
+          <div className="nursenavBar">
+              <Link to="/frontdeskHome" className='nurselink'>Weekly View</Link>
+              <Link to="/frontdeskSearch" className='nurselink'>Check In Patient</Link>
+          </div>
+      </div>
     </>
   );
 }
