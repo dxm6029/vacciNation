5c8f5c99be4034d38b056abcc07eb7ba1b5b7b34
lo6169 <lo6169@rit.edu>
2022-03-13 19:34:25 -0400

    Adding post/get stuff to different pages and modified one small thing in the swagger docs

--- frontend/mainapp/src/staffPages/nursePages/findPatient.js
+++ frontend/mainapp/src/staffPages/nursePages/findPatient.js
@@ -1,17 +1,55 @@
 import './patient.css';
 import NavBar from './navBar';
+import axios from 'axios';
 
 function FindPatient() {
     // Find a patient
+
+    const FIND = (event) => {
+      event.preventDefault();
+      console.log(event.target);
+      let firstName = event.target.fname.value;
+      let lastName = event.target.lname.value;
+      let dob = event.target.dob.value;
+      let emailAddress = event.target.email.value;
+      return axios
+        .get("/UserCitizen"), {
+          params: {
+            first_name: firstName,
+            last_name: lastName,
+            email: emailAddress
+          }
+        }
+        .then((response) => {
+            if (response) {
+                console.log(response); 
+            } else {
+                console.log('API failed: No data received!');
+                return null;
+            }
+        }).catch((err) => {
+            console.log('*** API Call Failed ***')
+            console.log(err)
+            console.log(err.toString())
+            return null;
+        });
+    }
+
   return (
     <>
         <NavBar />
 
         <h1>Find Patient</h1> 
 
-        <form className="patientForm">
-            <label htmlFor="name">Patient's Name</label>
-            <input type="text" id="name" name="name" placeholder="Name..."/>
+        <form className="patientForm" onSubmit={e => {FIND(e)}}>
+            <label htmlFor="fname">Patient's First Name</label>
+            <input type="text" id="fname" name="fname" placeholder="First Name..."/>
+
+            <label htmlFor="lname">Patient's Last Name</label>
+            <input type="text" id="lname" name="lname" placeholder="Last Name..."/>
+
+            <label htmlFor="email">Patient's Email</label>
+            <input type="text" id="email" name="email" placeholder="Email Address"/>
 
             <label htmlFor="dob"> Patient's Date of Birth</label>
             <input type="date" id="dob" name="dob" placeholder="mm/dd/yyyy"/>
