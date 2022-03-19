9196f5d3e8279427cdcf20a064697e6b3897ac54
lo6169 <lo6169@rit.edu>
2022-03-19 15:39:58 -0400

    Successfully able to get information from the DB

--- frontend/mainapp/src/pages/schedule.js
+++ frontend/mainapp/src/pages/schedule.js
@@ -1,21 +1,24 @@
 import './schedule.css';
-import { Link } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import NavBar from './navBar';
 import axios from 'axios';
 import { useState } from 'react';
+import { Navigation } from '@mui/icons-material';
 
 function Schedule() {
   const [list, setList] = useState(null);
+  const [datePick, setDatePick] = useState(null);
+  const [canSubmit, setCanSubmit] = useState(false);
+  const [locationId, setLocationId] = useState(null);
+  const [appt, setAppt] = useState(null);
+
+  const navigate = useNavigate();
 
   const FIND = (event) => {
     event.preventDefault();
-    console.log(event.target);
-    let datePick = event.target.datePick.value;
+    setDatePick(event.target.datePick.value);
 
-    console.log(datePick);
-    console.log("Before return");
-
-    return axios.get(`http://localhost:5002/Appointment/open/date?date=${datePick}`)
+    return axios.get(`http://localhost:5002/Appointment/open/date?date=${event.target.datePick.value}`)
       .then((response) => {
           if (response) {
               console.log(response); 
@@ -32,6 +35,42 @@ function Schedule() {
           return null;
       });
   };
+
+  function selectLocation(id) {
+    return axios.get(`http://localhost:5002/Appointment/date/open/${id}?date=${datePick}`)
+      .then((response) => {
+          if (response) {
+              console.log(response); 
+              setLocationId(response.data.location_id);
+              setCanSubmit(true);
+          } else {
+              console.log('API failed: No data received!');
+              return null;
+          }
+      }).catch((err) => {
+          console.log('*** API Call Failed ***');
+          console.log(err);
+          console.log(err.toString());
+          return null;
+      });
+  }
+
+  function nextPage() {
+    if (canSubmit) {
+      console.log("Log in succeed");
+      navigate('/personalInfo', {
+        locationId: locationId,
+        appt: appt
+      });
+
+    }
+    else {
+      // pop up an alert that says no
+      console.log("Log in failed");
+      alert("Please fill out all fields and select an appointment.");
+    }
+
+  }
 
   return (
     <div className="scheduler">
@@ -55,44 +94,50 @@ function Schedule() {
 
               <input type="submit" value="Search"/>
             </form>
-            
 
-            <table>
+            <table id="customers">
               <thead>
                 <tr>
                   <th>Location</th>
-                  <th>Distance</th>
                 </tr>
               </thead>
               <tbody>
                 {list && 
                   list.map((place, index) => (
-                    <tr className="" key={`location ${index}`}>
+                    <tr 
+                      className="" 
+                      key={`location ${index}`} 
+                      onClick={() => selectLocation(place.location_id)}
+                    >
                       <td>
                         {place.location_name}
                       </td>
                     </tr>
                   ))
                 }
-
               </tbody>
             </table>
-
-            <>
-              Calendar to go here <br/>
-            </>
-
-            <>
-              Select a time
-            </>
-            
         </div>
 
-        <Link to="/personalInfo">
-            <button> Next </button>
-        </Link>
+        <button onClick={() => nextPage()}> Next </button>
     </div>
   );
 }
 
 export default Schedule;
+
+
+/*
+
+{
+  "timeslot_id": 0,
+  "staff_id": 0,
+  "citizen_id": 0,
+  "location_id": 0,
+  "dose_id": 0,
+  "date": "string",
+  "status": 0,
+  "reactions": "string"
+}
+
+*/
