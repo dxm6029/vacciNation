11c71e569d7eb7e4e193d66f39bbfbde6178935a
dominique-molee <dxm6029@rit.edu>
2022-03-30 20:47:49 -0400

    stupid error 2

--- backend/VacciNationAPI/DataLayer/ReportManagement.cs
+++ backend/VacciNationAPI/DataLayer/ReportManagement.cs
@@ -585,7 +585,7 @@ namespace VacciNationAPI.DataLayer
                         Console.WriteLine(staff.last_name);
                         var fname = staff.first_name;  
                         var lname = staff.last_name;
-                        ReactionReport r = new ReactionReport(rdr.IsDBNull(0) ? -1 : rdr.GetInt32(0), rdr.IsDBNull(1) ? "" : rdr.GetString(1),rdr.IsDBNull(2) ? "" : rdr.GetString(2), rdr.IsDBNull(3) ? "" : rdr.GetString(3), rdr.IsDBNull(4) ? "" : rdr.GetString(4), rdr.IsDBNull(5) ? "" : rdr.GetString(5), "sally", "baker", rdr.IsDBNull(7) ? "" : rdr.GetString(7), rdr.IsDBNull(8) ? "" : rdr.GetString(8));
+                        ReactionReport r = new ReactionReport(rdr.IsDBNull(0) ? -1 : rdr.GetInt32(0), rdr.IsDBNull(1) ? "" : rdr.GetString(1),rdr.IsDBNull(2) ? "" : rdr.GetString(2), rdr.IsDBNull(3) ? "" : rdr.GetString(3), rdr.IsDBNull(4) ? "" : rdr.GetString(4), rdr.IsDBNull(5) ? "" : rdr.GetString(5), staff.first_name, staff.last_name, rdr.IsDBNull(7) ? "" : rdr.GetString(7), rdr.IsDBNull(8) ? "" : rdr.GetString(8));
                         Console.WriteLine(r.staff_first_name);
                         Console.WriteLine(r.staff_last_name);
                         appointments.Add(r);
